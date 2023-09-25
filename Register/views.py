02cdec05090d5be2bf9b73f03b6b97c8b6490ae1
6410685116 <6410685116@student.tu.ac.th>
2023-09-25 23:28:37 +0700

    Update WebApp

---
+++ Register/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world")
+
+def greet(request, name):
+    return render(request, 'hello/greet.html', {
+        "name": name.title()
+    })
+    
--- djangoWebApp/settings.py
+++ djangoWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zn(7ss%f5_k5kk9v5vv!&j!p)rjk77wocf07gs00h5+dzcd*h)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Register'
 ]
 
 MIDDLEWARE = [

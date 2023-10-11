ef78706ac116dbd1a32a2f899377b990cccc636e
6410685157 <6410685157@student.tu.ac.th>
2023-10-11 22:06:55 +0700

    update for deploy

--- djangoWebApp/settings.py
+++ djangoWebApp/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "Register/registrar"
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
